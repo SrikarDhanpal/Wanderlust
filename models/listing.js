eada226adba35c2eb5c3cdd01111ea6682b7bcee
Srikar Dhanpal <srikardhanpal0108@gmail.com>
2024-12-03 22:47:59 +0530

    first commit in reviews

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose= require("mongoose");
+const reviews = require("./reviews");
 
 
 const listingSchema= new mongoose.Schema({
@@ -27,7 +28,14 @@ const listingSchema= new mongoose.Schema({
       },
     price: Number,
     location: String,
-    country: String
+    country: String,
+    reviews: [
+      {
+
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ]
 });
 const Listing = mongoose.model("Listing",listingSchema);
 
